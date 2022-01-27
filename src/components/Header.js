5fee68a83efe72b5e138e0d1aa3f280aebafc531
amautacelt <amautacelt@gmail.com>
2022-01-26 20:45:31 -0500

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles/Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header__logo">
+            <h1>Leedd</h1>
+        </div>
+
+        <nav>
+            <ul>
+
+                <div className="closed">
+                    
+                </div>
+
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header;
