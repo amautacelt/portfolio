b13b7f67c501c8b55c61d9c6b2efc24144141d20
amautacelt <amautacelt@gmail.com>
2022-01-27 21:26:13 -0500

    menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
+import { Close, MenuOutlined } from '@material-ui/icons';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles/Header.css';
 
 const Header = () => {
@@ -12,11 +14,40 @@ const Header = () => {
             <ul>
 
                 <div className="closed">
-                    
+                    <Close className='close'/>
                 </div>
+
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+
+                <li>
+                    <Link to='/'>About</Link>
+                </li>   
+
+                <li>
+                    <Link to='/'>Portfolio</Link>
+                </li>
+
+                <li>
+                    <Link to='/'>Pages</Link>
+                </li>   
+
+                <li>
+                    <Link to='/'>Blog</Link>
+                </li>
+
+                <li>
+                    <Link to='/'>Contact</Link>
+                </li>               
 
             </ul>
         </nav>
+
+        <div className="changer">
+            <MenuOutlined className='menu' />
+        </div>
+
     </div>
   )
 }
