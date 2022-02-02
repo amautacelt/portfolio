06f94a4b62ece6803ca71544989458d6cb8ad0b6
amautacelt <amautacelt@gmail.com>
2022-02-01 22:04:16 -0500

    icons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,33 @@
+import { Facebook, Instagram, LinkedIn, Pinterest, Twitter } from '@material-ui/icons';
 import React from 'react';
 import './styles/Main.css';
 
 const Main = () => {
   return (
-    <div>
+    <div className='main'>
+
+        <div className="main__container">
+
+            <div className="text">
+                <p>Hey There</p>
+                <h1>I am Bob Russer</h1>
+                <p>Create Art Director & Designer</p>
+
+
+            <div className="icons">
+                <Twitter />
+                <Instagram />
+                <Facebook />
+                <LinkedIn />
+                <Pinterest />
+            </div>
+
+
+
+            </div>
+
+
+        </div>
 
     </div>
   )
